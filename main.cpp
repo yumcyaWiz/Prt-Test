7614bfabb7a35f5b031bc4b8a6890953ff9181e9
yumcyawiz <sergeyinverse@gmail.com>
2018-06-11 15:58:39 +0900

    add obj loader

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <vector>
+#include <cstdlib>
 #include "vec3.h"
 #include "math.h"
 #include "sky.h"
 #include "image.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
 
 
 std::random_device rnd_dev;
@@ -95,6 +100,53 @@ void ProjectLightFunction(Vec3* coeffs, Sampler* sampler, Sky* sky, int bands) {
     float weight = 4.0f*M_PI / sampler->n;
     for(int i = 0; i < sampler->n; i++) {
         coeffs[i] = coeffs[i] * weight;
+    }
+}
+
+
+struct Triangle {
+    int v1;
+    int v2;
+    int v3;
+};
+struct Scene {
+    Vec3* vertices;
+    Vec3* normals;
+    Triangle* triangles;
+    int vertices_n;
+};
+
+
+void loadObj(const std::string& filename, Vec3* vertices, Vec3* normals, Triangle* triangles) {
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+
+    std::string err;
+    bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, filename.c_str());
+    if(!err.empty()) {
+        std::cerr << err << std::endl;
+    }
+    if(!ret) {
+        std::exit(1);
+    }
+
+    for(size_t s = 0; s < shapes.size(); s++) {
+        size_t index_offset = 0;
+        for(size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+            int fv = shapes[s].mesh.num_face_vertices[f];
+            for(size_t v = 0; v < fv; v++) {
+                tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+                tinyobj::real_t vx = attrib.vertices[3*idx.vertex_index+0];
+                tinyobj::real_t vy = attrib.vertices[3*idx.vertex_index+1];
+                tinyobj::real_t vz = attrib.vertices[3*idx.vertex_index+2];
+                
+                tinyobj::real_t nx = attrib.normals[3*idx.normal_index+0];
+                tinyobj::real_t ny = attrib.normals[3*idx.normal_index+1];
+                tinyobj::real_t nz = attrib.normals[3*idx.normal_index+2];
+            }
+            index_offset += fv;
+        }
     }
 }
 
